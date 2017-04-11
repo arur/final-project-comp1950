c995aefc0c2228c780204a9bd6f28a11febead36
Artur Hayrapetyan <hayarur@gmail.com>
2017-04-10 23:00:52 -0700

    Add footer.

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ $(function () {
   var $window = $(window);
   var offset = $sidebar.offset();
   var topPadding = 15;
+  var mTop = 0;
   var isMobile = window.matchMedia('only screen and (max-width: 760px)');
 
-  if (!isMobile.matches) {
+  if (!isMobile.matches && $sidebar.width() <= 255) {
     $window.scroll(function () {
-      if ($window.scrollTop() > offset.top) {
+      if ($(window).scrollTop() + $(window).height() > $(document).height() - 150) {
+        $sidebar.stop();
+      } else if ($window.scrollTop() > offset.top) {
         $sidebar.stop().animate({
           marginTop: $window.scrollTop() - offset.top + topPadding
-        });
+        }, 600);
       } else {
         $sidebar.stop().animate({
           marginTop: 0
-        });
+        }, 600);
       }
     });
   }
